59ae1435ceef45ea88c06325da492481b355ba59
Keith C. Ivey <keith@iveys.org>
2019-03-02 12:29:47 -0500

    tweak tests

--- test/test.js
+++ test/test.js
@@ -1,57 +1,74 @@
 const assert = require('assert');
 const NumberList = require('..');
 describe(
-    '#stringify',
+    '.stringify()',
     function () {
-        const tests = [
-            [[1], '1'],
-            [[1, 2, 3], '1-3'],
-            [[3, 1, 2], '1-3'],
-        ];
-        for (const [arg, expected] of tests) {
-            it(
-                `${JSON.stringify(arg)} should become "${expected}"`,
-                function () {
-                    assert.strictEqual(NumberList.stringify(arg), expected);
+        describe(
+            'return values',
+            function () {
+                const tests = [
+                    [[1], '1'],
+                    [[1, 2, 3], '1-3'],
+                    [[3, 1, 2], '1-3'],
+                    [[], ''],
+                ];
+                for (const [arg, expected] of tests) {
+                    it(
+                        `${JSON.stringify(arg)} should become "${expected}"`,
+                        function () {
+                            assert.strictEqual(NumberList.stringify(arg), expected);
+                        }
+                    );
                 }
-            );
-        }
+            }
+        );
     }
 );
 describe(
-    '#parse',
+    '.parse()',
     function () {
-        const tests = [
-            ['1', [1]],
-            ['1-3', [1, 2, 3]],
-            ['2-5,7', [2, 3, 4, 5, 7]],
-            [' 4 9\t7,', [4, 7, 9]],
-        ];
-        for (const [arg, expected] of tests) {
-            it(
-                `"${arg}" should become ${JSON.stringify(expected)}`,
-                function () {
-                    assert.deepStrictEqual(NumberList.parse(arg), expected);
-                }
-            );
-        }
-        const errorTests = [
-            ['-1', /Invalid number list/],
-            ['a', /Invalid number list/],
-            ['7-5', /Invalid range/],
-        ];
-        for (const [arg, regexp] of errorTests) {
-            it(
-                `"${arg}" should throw ${regexp} error`,
-                function () {
-                    assert.throws(
+        describe(
+            'return values',
+            function () {
+                const tests = [
+                    ['1', [1]],
+                    ['1-3', [1, 2, 3]],
+                    ['2-5,7', [2, 3, 4, 5, 7]],
+                    [' 4 9\t7,', [4, 7, 9]],
+                    ['', []],
+                ];
+                for (const [arg, expected] of tests) {
+                    it(
+                        `"${arg}" should become ${JSON.stringify(expected)}`,
                         function () {
-                            NumberList.parse(arg);
-                        },
-                        regexp
+                            assert.deepStrictEqual(NumberList.parse(arg), expected);
+                        }
                     );
                 }
-            );
-        }
+            }
+        );
+        describe(
+            'errors',
+            function () {
+                const tests = [
+                    ['-1', /Invalid number list/],
+                    ['a', /Invalid number list/],
+                    ['7-5', /Invalid range/],
+                ];
+                for (const [arg, regexp] of tests) {
+                    it(
+                        `"${arg}" should throw ${regexp} error`,
+                        function () {
+                            assert.throws(
+                                function () {
+                                    NumberList.parse(arg);
+                                },
+                                regexp
+                            );
+                        }
+                    );
+                }
+            }
+        );
     }
 );
